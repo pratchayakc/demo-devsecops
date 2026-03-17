68d7058c66b280e1451dfbdcbb1621a74976f15b
pratchayakc <pratchaya.kc@gmail.com>
2026-03-17 19:56:16 +0700

    update kong

--- app/index.js
+++ app/index.js
@@ -2,7 +2,14 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello World from k3s 🚀\n");
+
+  if (req.url.startsWith("/page1")) {
+    res.end("hello from page 1\n");
+  } else if (req.url.startsWith("/page2")) {
+    res.end("hello from page 2\n");
+  } else {
+    res.end("hello from root\n");
+  }
 });
 
 server.listen(3000, "0.0.0.0", () => {
