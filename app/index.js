9c2ef01512bb22e52b6dbab7bba4e05c464db9d3
pratchayakc <89010968+pratchayakc@users.noreply.github.com>
2026-03-19 16:12:22 +0700

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,14 +1,23 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
+  const date = new Date();
+  const timestamp = `${date.toISOString()}`;  // ใช้ format ISO 8601
+
   res.writeHead(200, { "Content-Type": "text/plain" });
 
   if (req.url.startsWith("/page1")) {
-    res.end("hello from page 1\n");
+    const message = "hello from page 1";
+    console.log(`[${timestamp}] INFO: ${message}`);
+    res.end(message + "\n");
   } else if (req.url.startsWith("/page2")) {
-    res.end("hello from page 2\n");
+    const message = "hello from page 2";
+    console.log(`[${timestamp}] INFO: ${message}`);
+    res.end(message + "\n");
   } else {
-    res.end("hello from root\n");
+    const message = "hello from root";
+    console.log(`[${timestamp}] INFO: ${message}`);
+    res.end(message + "\n");
   }
 });
 
